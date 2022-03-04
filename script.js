6a00057abe08a3dc13eedd00bacb8796c34f5eb7
emily le <emilytwo19@comcast.net>
2022-03-04 15:26:39 -0800

    updated code to be reviewed by Stephen

--- script.js
+++ script.js
@@ -3,3 +3,27 @@
 function restart() {
  window.location.reload();
 }
+
+//Hides and shows the dropdown content
+function myDurationDropdown() {
+ document.getElementById("myDurationDropdown").classList.toggle("show");
+}
+
+function myTypeDropdown() {
+ document.getElementById("myTypeDropdown").classList.toggle("show");
+}
+
+// Closes dropdown if misclicked
+window.onclick = function(event) {
+ if (!event.target.matches('.dropbtn')) {
+  var dropdown = document.getElementsByClassName("dropdown-content");
+  var i;
+  for (i = 0; i < dropdown.length(); i++) {
+   var openDropdown = dropdown[i];
+   if (openDropdown.classList.contains('show')) {
+    openDropdown.classList.remove('show');
+   }
+  }
+ }
+}
+
